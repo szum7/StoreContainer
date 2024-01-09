f2b8710d8769b536fb71bcc2481da4e78fa51f2d
aronszocs <aron.szocs.ext@sodexo.com>
2024-01-09 12:17:40 +0100

    init

---
+++ StoreContainerApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StoreContainerApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("END");
+        }
+    }
+}
